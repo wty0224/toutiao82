65ee084da50eac6fb691055797f9b2d92251f5f5
王天阳 <wty0224@outlook.com>
2019-08-31 20:37:21 +0800

    写好静态页面

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
-
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    login
+  <div class="login">
+    <el-card class="login-card">
+      <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt="" />
+      </div>
+      <el-form ref="form" >
+        <el-form-item>
+          <el-input placeholder="请输入手机号" style="margin-top:20px"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:250px"></el-input>
+          <el-button type="primary" style="float:right">获取验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+        <el-checkbox >是否同意</el-checkbox>
+        </el-form-item>
+        <el-form-item style="text-align: center">
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang = 'less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 350px;
+    .logo {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
+  }
+}
 </style>
