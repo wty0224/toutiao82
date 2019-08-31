9e392062662fe56b741dbd2e45e7ad21c6856d04
王天阳 <wty0224@outlook.com>
2019-08-31 19:55:14 +0800

    项目模板

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
